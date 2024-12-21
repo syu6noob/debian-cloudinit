82dfbbbab5cacb7a9f7d16d871c8b4bcf85666cd
syu6 <kusahebi@outlook.jp>
2024-12-21 19:32:35 +0900

    Update host.sh
    - add memory and storage size setting
    - remove the downloaded image at the end of this script

--- host.sh
+++ host.sh
@@ -18,9 +18,15 @@ sudo wget -P /root/ https://cdimage.debian.org/images/cloud/bookworm/latest/debi
 # create vm
 sudo qm create ${VM_ID} --name ${VM_NAME} --net0 virtio,bridge=vmbr0
 sudo qm importdisk ${VM_ID} ./debian-12-generic-amd64.qcow2 ${HOST_DISK}
-sudo qm set ${VM_ID} --scsihw virtio-scsi-pci --scsi0 ${HOST_DISK}:vm-${VM_ID}-disk-0
+sudo qm set ${VM_ID} --scsihw virtio-scsi-pci --scsi0 ${HOST_DISK}:vm-${VM_ID}-disk-0,discard=on
 sudo qm set ${VM_ID} --ide2 ${HOST_DISK}:cloudinit
 sudo qm set ${VM_ID} --serial0 socket
+sudo qm set ${VM_ID} --ostype l26
+sudo qm set ${VM_ID} --memory 2048
 sudo qm set ${VM_ID} --boot c --bootdisk scsi0
+sudo qm set ${VM_ID} scsi0 16G
 sudo qm set ${VM_ID} --agent enabled=1
 sudo qm template ${VM_ID}
+
+# remove the image
+sudo rm ./debian-12-generic-amd64.qcow2
