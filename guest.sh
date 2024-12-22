3810f311eb4370643588ebda303beda4bc0ae80b
syu6 <kusahebi@outlook.jp>
2024-12-22 17:06:06 +0900

    Update guest.sh
    comment out `sudo systemctl enable qemu-guest-agent` beacuse there is no need to execute this command

--- guest.sh
+++ guest.sh
@@ -5,7 +5,6 @@ password="password"
 
 sudo apt -y install qemu-guest-agent
 sudo systemctl start qemu-guest-agent
-sudo systemctl enable qemu-guest-agent
 
 sudo timedatectl set-timezone Asia/Tokyo
 
