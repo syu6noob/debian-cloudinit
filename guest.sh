e4010a7c8b4352edaa582cf29056d2605797fa30
syu6 <kusahebi@outlook.jp>
2024-12-22 16:47:49 +0900

    Update guest.sh
    Add code-servser setting

--- guest.sh
+++ guest.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 set -euo pipefail
 
+$password="password"
+
 sudo apt -y install qemu-guest-agent
 sudo systemctl start qemu-guest-agent
 sudo systemctl enable qemu-guest-agent
 
 sudo timedatectl set-timezone Asia/Tokyo
+
+while (( $# > 0 ))
+do
+  case $1 in
+    # ...
+    --code)
+      # For code-server
+      sudo curl -fsSL https://code-server.dev/install.sh | sh
+      
+      cat <<EOF > "/root/.config/code-server/config.yaml"
+bind-addr: 0.0.0.0:8080
+auth: password
+password: $password
+cert: false
+EOF
+      
+      sudo systemctl enable --now code-server@root
+      sudo systemctl start --now code-server@root
+      ;;
+    # ...
+  esac
+  shift
+done
